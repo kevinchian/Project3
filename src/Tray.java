7566a735199a797afbb46c90112984bda0e0ecd4
kevinchian <kchian2006@gmail.com>
2013-08-10 15:33:32 -0700

    Added equals method

--- src/Tray.java
+++ src/Tray.java
@@ -5,15 +5,15 @@ public class Tray {
 	private static boolean iAmDebugging = true;
 	
 	private int[][] tray;
-	private HashSet<Block> blocks;
+	private HashMap<Integer, Block> blocks;
 	
 	public Tray(int trayHeight, int trayWidth){
 		tray = new int[trayHeight][trayWidth];
-		blocks = new HashSet<Block>();
+		blocks = new HashMap<Integer, Block>();
 	}
 	
 	public void add(Block b){
-		blocks.add(b);
+		blocks.put(b.id(), b);
 		for(int i=b.top(); i<b.bottom(); i++)
 			for(int j=b.top(); j<b.bottom(); j++)
 				tray[i][j] = b.id();
@@ -32,9 +32,52 @@ public class Tray {
 		return "";
 	}
 	
-	public boolean equals(Object o){
-		// kevin
-		// make sure to account for wildcards
+	// Iterates through the tray to see if the equals are the same.
+	public boolean equals(Tray compare){
+		if(this.tray.length != compare.tray.length){
+			return false;
+		}
+		if(this.tray[0].length != compare.tray[0].length){
+			return false;
+		}
+		for(int x = 0; x < tray.length; x++){
+			for(int y = 0; y<tray[x].length; y++){
+				int thisID = this.tray[x][y];
+				int otherID = compare.tray[x][y];
+				if(otherID == -1 || thisID == -1){ 
+					//skip to next iteration: wild card
+				}
+				else if(thisID == 0 && otherID != 0 || otherID == 0 && thisID != 0){
+					return false;
+				}
+				else{
+					Block b1 = blocks.get(thisID);
+					Block b2 = compare.blocks.get(otherID);
+					if(b1.top() != b2.top() || b1.bottom() != b2.bottom() || b1.left() != b2.left() 
+							|| b1.right() != b2.right()){ // Maybe write a block equals method testing its coordinates but not hashcode.
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+	
+	// Potentially faster equals method
+	public boolean equals2(Tray compare){
+		if(this.tray.length != compare.tray.length){
+			return false;
+		}
+		if(this.tray[0].length != compare.tray[0].length){
+			return false;
+		}
+		for(int x = 0; x < tray.length; x++){
+			while(true){
+				
+			}
+		}
 		return false;
 	}
+	
+	
 }
